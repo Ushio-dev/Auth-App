1aaac9825fe33c97098604ba51ecdca7e5e2b157
Franco <fran14korn@live.com>
2023-12-19 11:51:31 -0300

    se crea pantalla de registro

--- app/src/main/java/com/example/authapp/screens/Register.kt
+++ app/src/main/java/com/example/authapp/screens/Register.kt
@@ -3,35 +3,62 @@ package com.example.authapp.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.authapp.LoginError
+import com.example.authapp.components.EmailTextInput
+import com.example.authapp.components.PasswordTextInput
 
 @Composable
 fun Register(navController: NavHostController) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Texto()
-        Spacer(modifier = Modifier.height(15.dp))
-        // Pasar controller por parametros, pasando la funcion que haga el trabajo de navegacion no funciona, nose porque
-        BackButton(navController)
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    var repeatedPassword by remember {
+        mutableStateOf("")
+    }
+
+    var emailError by remember {
+        mutableStateOf(LoginError())
+    }
+
+    var errorPassword by remember {
+        mutableStateOf(LoginError())
+    }
+
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            EmailTextInput(email = email, updateEmail = { email = it }, emailError = emailError)
+            Spacer(modifier = Modifier.height(20.dp))
+            PasswordTextInput(
+                password = password,
+                updatePassword = { password = it },
+                errorPassword = errorPassword
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            PasswordTextInput(
+                password = repeatedPassword,
+                updatePassword = { repeatedPassword = it },
+                errorPassword = errorPassword
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+            
+        }
     }
 }
-
-@Composable
-fun Texto() {
-    Text(text = "Hola Mundo")
-}
-
-@Composable
-fun BackButton(navController: NavHostController) {
-    Button(onClick = { navController.navigate("LOGIN") }) {
-        Text(text = "Volver a Login")
-    }
-}
-
